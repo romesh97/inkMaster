7d31d3fd7e02ae48d5528f85fbcc95fe534df8d8
Manthika <tharumudu@gmail.com>
2019-05-01 02:58:31 +0530

    feature matching implemented
    We were using template matching and we tried feature matching method to get better results in real-time.

---
+++ AR/OpenCV/inkMaster/test.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import imutils
+MIN_MATCH_COUNT = 30
+
+detector = cv2.xfeatures2d.SIFT_create()
+FLANN_INDEX_KDITREE = 0
+flannParam = dict(algorithm=FLANN_INDEX_KDITREE, tree=5)
+flann = cv2.FlannBasedMatcher(flannParam, {})
+
+trainImg = cv2.imread("C:\\Users\\Manthika\\Desktop\\opencvtest\\templates\\template1.jpg", 0)
+trainImg = cv2.Canny(trainImg, 50, 140)
+trainImg  = imutils.resize(trainImg, width=50)
+trainKP, trainDesc = detector.detectAndCompute(trainImg, None)
+
+cam = cv2.VideoCapture(0)
+while True:
+    ret, QueryImgBGR = cam.read()
+    img = cv2.cvtColor(QueryImgBGR, cv2.COLOR_BGR2GRAY)
+    QueryImg = cv2.Canny(img, 50, 140)
+    queryKP, queryDesc = detector.detectAndCompute(QueryImg, None)
+    matches = flann.knnMatch(queryDesc, trainDesc, k=2)
+
+    goodMatch=[]
+    for m, n in matches:
+
+        if(m.distance<0.75*n.distance):
+            goodMatch.append(m)
+    if(len(goodMatch)>MIN_MATCH_COUNT):
+        tp = []
+        qp = []
+        for m in goodMatch:
+            tp.append(trainKP[m.trainIdx].pt)
+            qp.append(queryKP[m.queryIdx].pt)
+        tp, qp = np.float32((tp, qp))
+        H, status = cv2.findHomography(tp, qp, cv2.RANSAC, 3.0)
+        h, w = trainImg.shape
+        trainBorder = np.float32([[[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]])
+        queryBorder = cv2.perspectiveTransform(trainBorder, H)
+        cv2.polylines(QueryImgBGR, [np.int32(queryBorder)], True, (0, 255, 0), 5)
+    else:
+        print("Not Enough match found- %d/%d"%(len(goodMatch), MIN_MATCH_COUNT))
+    cv2.imshow('result', QueryImgBGR)
+    if cv2.waitKey(10) == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()

d52a32edf6671f640685bbd6fea261316e43f7b6
Manthika <tharumudu@gmail.com>
2019-05-02 18:10:43 +0530

    Added the openCV part again after deleting to avoid conflicts

---
+++ AR/OpenCV/inkMaster/1.py
@@ -0,0 +1,81 @@
+import cv2 as cv2
+import numpy as np
+import imutils
+
+
+def main():
+
+    template1 = cv2.imread("C:\\Users\\Manthika\\Desktop\\opencvtest\\template.jpg")
+    template1 = cv2.cvtColor(template1, cv2.COLOR_BGR2GRAY)
+    template1 = cv2.Canny(template1, 50, 200)
+    template = imutils.resize(template1, width=60)
+    (tH, tW) = template.shape[:2]
+    cv2.imshow("Template", template)
+
+    windowName = "Something"
+    cv2.namedWindow(windowName)
+    cap = cv2.VideoCapture(0)
+
+    if cap.isOpened():
+        ret, frame = cap.read()
+    else:
+        ret = False
+
+    # loop over the frames to find the template
+    while ret:
+        # load the image, convert it to grayscale, and initialize the
+        # bookkeeping variable to keep track of the matched region
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        found = None
+
+        # loop over the scales of the image
+        for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+            # resize the image according to the scale, and keep track
+            # of the ratio of the resizing
+            resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+            r = gray.shape[1] / float(resized.shape[1])
+
+            # if the resized image is smaller than the template, then break
+            # from the loop
+            if resized.shape[0] < tH or resized.shape[1] < tW:
+                print("frame is smaller than the template")
+                break
+
+            # detect edges in the resized, grayscale image and apply template
+            # matching to find the template in the image
+            edged = cv2.Canny(resized, 50, 200)
+            result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+            (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+            # if we have found a new maximum correlation value, then update
+            # the bookkeeping variable
+            if found is None or maxVal > found[0]:
+                found = (maxVal, maxLoc, r)
+
+            # unpack the bookkeeping variable and compute the (x, y) coordinates
+            # of the bounding box based on the resized ratio
+        # print(found)
+        if found is None:
+            # just show only the frames if the template is not detected
+            cv2.imshow(windowName, frame)
+            print("No template is found")
+        else:
+            (_, maxLoc, r) = found
+            (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+            (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+            print(startX, startY, endX, endY)
+
+            # draw a bounding box around the detected result and display the image
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+            cv2.imshow(windowName, frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+if __name__ == "__main__":
+    main()

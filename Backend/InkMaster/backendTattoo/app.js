706a701db278c492a09981959c1afccc14edce1d
Achintha <kalanaachintha@gmail.com>
2019-05-04 02:07:07 +0530

    Backend updating

---
+++ Backend/InkMaster/backendTattoo/app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose=require('mongoose')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb://127.0.0.1:27017/recommends',{useNewUrlParser:true });
+const connection=mongoose.connection;
+
+connection.once('open',function () {
+  console.log("MongoDB database Connection is successful")
+
+})
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

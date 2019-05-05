2bdbd5e561b527eed0b096f7c10d4178e3f4791c
AmnaHilmy07 <noor.2017466@iit.ac.lk>
2019-05-05 23:20:08 +0530

    Update SignUpScreen.js

--- Frontend/inkMaster/app/pages/screens/SignUpScreen.js
+++ Frontend/inkMaster/app/pages/screens/SignUpScreen.js
@@ -4,14 +4,58 @@ import {
   Text,
   TextInput,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert
 } from 'react-native';
 
 import Logo from 'inkMaster/app/components/Logo.js';
 
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+// import firebase from 'firebase';
+
+import firebase from 'react-native-firebase';
 
 export default class Signup extends React.Component {
+  
+  // static navigationOptions = ({ navigation }) => {
+  //   return {
+  //     title: navigation.getParam('type', 'DEFAULT_TYPE'),
+  //   };
+  // };
+
+constructor(props){
+  super(props);
+  this.state = {
+    usernameText : '',
+    emailText : '',
+    contactText : '',
+    passwordText : '',
+    loading:false,
+    isLoggedIn: false,
+    errorText: ''
+  }
+  this.artists = firebase.firestore().collection('artists');
+  this.customers = firebase.firestore().collection('customers');
+  this._signUp = this._signUp.bind(this);
+}
+
+    // componentWillMount() {
+    //   var config = {
+    //     apiKey: "AIzaSyCGru61A858SfGMVhhbcvN8J-rrCRueWwU",
+    //     authDomain: "inkmasterapps.firebaseapp.com",
+    //     databaseURL: "https://inkmasterapps.firebaseio.com",
+    //     projectId: "inkmasterapps",
+    //     storageBucket: "inkmasterapps.appspot.com",
+    //     messagingSenderId: "987937825635"
+    //   };
+    //   firebase.initializeApp(config);
+    //   firebase.auth().onAuthStateChanged(user=>{
+    //     if(user) {
+    //       this.setState({isLoggedIn: !this.state.isLoggedIn})
+    //     } else {
+    //       this.setState({isLoggedIn: false})
+    //     }
+    // })
+    // }
 
     render() {
       return(
@@ -20,11 +64,11 @@ export default class Signup extends React.Component {
             <View style={styles.container}>
               <TextInput style={styles.inputBox} 
                   underlineColorAndroid='rgba(0,0,0,0)' 
-                  placeholder="Name"
+                  placeholder="Username"
                   placeholderTextColor = "#ffffff"
                   selectionColor="#fff"
                   keyboardType="default"
-                  onSubmitEditing={()=> this.password.focus()}
+                  onChangeText={text=>this.setState({usernameText: text})}
                   />
               <TextInput style={styles.inputBox} 
                   underlineColorAndroid='rgba(0,0,0,0)' 
@@ -32,32 +76,25 @@ export default class Signup extends React.Component {
                   placeholderTextColor = "#ffffff"
                   selectionColor="#fff"
                   keyboardType="email-address"
-                  onSubmitEditing={()=> this.password.focus()}
+                  onChangeText={text=>this.setState({emailText: text})}
                   />
               <TextInput style={styles.inputBox} 
                   underlineColorAndroid='rgba(0,0,0,0)' 
                   placeholder="Contact"
                   placeholderTextColor = "#ffffff"
                   selectionColor="#fff"
-                  keyboardType="email-address"
-                  onSubmitEditing={()=> this.password.focus()}
+                  keyboardType="number-pad"
+                  onChangeText={text=>this.setState({contactText: text})}
                   />
               <TextInput style={styles.inputBox} 
                   underlineColorAndroid='rgba(0,0,0,0)' 
                   placeholder="Password"
                   secureTextEntry={true}
                   placeholderTextColor = "#ffffff"
-                  ref={(input) => this.password = input}
-                  />  
-              <TextInput style={styles.inputBox} 
-                  underlineColorAndroid='rgba(0,0,0,0)' 
-                  placeholder="Confirm password"
-                  secureTextEntry={true}
-                  placeholderTextColor = "#ffffff"
-                  ref={(input) => this.password = input}
-                  /> 
+                  onChangeText={text=>this.setState({passwordText: text})}
+                  />
               <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText} onPress={this._showSignIn}>Sign up</Text>
+                <Text style={styles.buttonText} onPress={this._signUp}>Sign up</Text>
               </TouchableOpacity>     
             </View>
             <View style={styles.signupTextCont}>
@@ -73,6 +110,99 @@ export default class Signup extends React.Component {
   _showSignIn = () => {
       this.props.navigation.navigate('SignIn');
   };
+
+  _signUp = async () => {
+      const response = await AsyncStorage.getItem('AccountType');
+    // Alert.alert(
+    //     'Registration Nearly Complete',
+    //     'Are you sure you would like to submit this form?',
+    //     [
+    //       {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+    //       {text: 'OK', onPress: () => {
+    //         console.log('Signup Completed!')
+    //               //signup function
+    //         this.props.navigation.navigate('SignIn');
+    //       }},
+    //     ],
+    //       { cancelable: false }
+    //     )
+    this.setState({loading: true});
+      const {emailText, passwordText} = this.state;
+      firebase.auth().createUserWithEmailAndPassword(emailText, passwordText)
+      .then(this.setState({
+        errorText: 'Success',
+        loading: true,
+        emailText: '',
+        passwordText: '',
+        usernameText: '',
+        contactText: '',
+        signedUp: true
+      }))
+      .catch(this.setState({
+        errorText: 'Authentication failed',
+        loading: true,
+        emailText: '',
+        passwordText: '',
+        usernameText: '',
+        contactText: '',
+      }))
+
+      if(signedUp) {
+          if(response == 'Customer') {
+            this.customers.add({
+                username: this.state.usernameText,
+                email: this.state.emailText,
+                contact: this.state.contactText
+            }).then(
+              this.setState({
+                errorText: 'Success',
+                loading: true,
+                emailText: '',
+                passwordText: '',
+                usernameText: '',
+                contactText: ''
+              })
+            ).catch(
+              this.setState({
+                errorText:'Success',
+                loading: true,
+                emailText: '',
+                passwordText: '',
+                usernameText: '',
+                contactText: ''
+              })
+            )
+          }  
+          else if(response == 'Artist'){
+            this.artists.add({
+                username: this.state.usernameText,
+                email: this.state.emailText,
+                contact: this.state.contactText
+            }).then(
+              this.setState({
+                errorText: 'Success',
+                loading: true,
+                emailText: '',
+                passwordText: '',
+                usernameText: '',
+                contactText: ''
+              })
+            ).catch(
+              this.setState({
+                errorText:'Success',
+                loading: true,
+                emailText: '',
+                passwordText: '',
+                usernameText: '',
+                contactText: ''
+              })
+            )
+          } 
+          
+        this.props.navigation.navigate('SignIn');
+
+      }
+  }
 
 }
 

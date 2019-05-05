a137a6d3d0e410cb045fd980241a354173a81315
AmnaHilmy07 <noor.2017466@iit.ac.lk>
2019-05-05 13:45:36 +0530

    Update AppNavigator.js

--- Frontend/inkMaster/app/pages/AppNavigator.js
+++ Frontend/inkMaster/app/pages/AppNavigator.js
@@ -1,23 +1,101 @@
 import React from 'react';
 import { 
   createSwitchNavigator, 
-  createStackNavigator, 
+  createStackNavigator,
+  createBottomTabNavigator, 
   createAppContainer 
 } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import AuthLoading from './screens/AuthLoading';
 import AccountTypeScreen from './screens/AccountTypeScreen';
 import SignInScreen from './screens/SignInScreen';
 import SignUpScreen from './screens/SignUpScreen';
-import HomeScreen from './screens/HomeScreen';
+// import HomeScreen from './screens/HomeScreen';
 import GallerySample from './screens/GallerySample';
 import UpdateAccInfo from './screens/UpdateAccInfo';
+import ArtistHomeScreen from './screens/Artist/ArtistHomeScreen';
+import CustomerHomeScreen from './screens/Customer/CustomerHomeScreen';
 
-const AppStack = createStackNavigator(
+//stack navigator
+// const CustomerAppStack = createStackNavigator(
+//   { 
+//     CustomerHome: CustomerHomeScreen, 
+//     Gallery: GallerySample,
+//     Settings: UpdateAccInfo
+//   }
+// );
+
+// const ArtistAppStack = createStackNavigator(
+//   { 
+//     ArtistHome: ArtistHomeScreen, 
+//     Gallery: GallerySample,
+//     Settings: UpdateAccInfo
+//   }
+// );
+
+//tab navigator
+const CustomerAppStack = createBottomTabNavigator(
   { 
-    Home: HomeScreen, 
+    Home: CustomerHomeScreen, 
     Gallery: GallerySample,
-    UpdateAccInfo: UpdateAccInfo 
+    Settings: UpdateAccInfo
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        // if (routeName === 'Home') {
+        //   iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        //   // Sometimes we want to add badges to some icons. 
+        //   // You can check the implementation below.
+        //   IconComponent = HomeIconWithBadge; 
+        // } else if (routeName === 'Settings') {
+        //   iconName = `ios-options`;
+        // }
+
+        // // You can return any component that you like here!
+        // return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
+
+const ArtistAppStack = createBottomTabNavigator(
+  { 
+    Home: ArtistHomeScreen, 
+    Gallery: GallerySample,
+    Settings: UpdateAccInfo
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        // if (routeName === 'Home') {
+        //   iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        //   // Sometimes we want to add badges to some icons. 
+        //   // You can check the implementation below.
+        //   IconComponent = HomeIconWithBadge; 
+        // } else if (routeName === 'Settings') {
+        //   iconName = `ios-options`;
+        // }
+
+        // // You can return any component that you like here!
+        // return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
   }
 );
 
@@ -34,7 +112,8 @@ export default createAppContainer(
   createSwitchNavigator(
     {
       AuthLoading: AuthLoading,
-      App: AppStack,
+      CustomerApp: CustomerAppStack,
+      ArtistApp: ArtistAppStack,
       Auth: AuthStack,
     },
     {

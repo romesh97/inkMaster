06eeb9fa661216357087d5c7fd015019da6ce4b5
JehiraXavier <43947403+JehiraXavier@users.noreply.github.com>
2019-05-05 06:43:36 +0530

    minor changes

--- Frontend/inkMaster/app/pages/AppNavigator.js
+++ Frontend/inkMaster/app/pages/AppNavigator.js
@@ -1,45 +1,39 @@
-import React from 'react';
-import { 
-  createSwitchNavigator, 
-  createStackNavigator, 
-  createAppContainer 
-} from 'react-navigation';
+import React from "react";
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
 
-import AuthLoading from './screens/AuthLoading';
-import AccountTypeScreen from './screens/AccountTypeScreen';
-import SignInScreen from './screens/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import HomeScreen from './screens/HomeScreen';
-import GallerySample from './screens/GallerySample';
-import UpdateAccInfo from './screens/UpdateAccInfo';
+import AuthLoading from "./screens/AuthLoading";
+import AccountTypeScreen from "./screens/AccountTypeScreen";
+import SignInScreen from "./screens/SignInScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import HomeScreen from "./screens/HomeScreen";
+import GallerySample from "./screens/GallerySample";
+import UpdateAccInfo from "./screens/UpdateAccInfo";
 
-const AppStack = createStackNavigator(
-  { 
-    Home: HomeScreen, 
-    Gallery: GallerySample,
-    UpdateAccInfo: UpdateAccInfo 
-  }
-);
+const AppStack = createStackNavigator({
+  Home: HomeScreen,
+  Gallery: GallerySample,
+  UpdateAccInfo: UpdateAccInfo
+});
 
-const AuthStack = createStackNavigator(
-  {
-    AccountType : AccountTypeScreen, 
-    SignIn: SignInScreen, 
-    SignUp :  SignUpScreen 
-  }
-);
+const AuthStack = createStackNavigator({
+  AccountType: AccountTypeScreen,
+  SignIn: SignInScreen,
+  SignUp: SignUpScreen
+});
 
 export default createAppContainer(
-
   createSwitchNavigator(
     {
       AuthLoading: AuthLoading,
       App: AppStack,
-      Auth: AuthStack,
+      Auth: AuthStack
     },
     {
-      initialRouteName: 'AuthLoading',
+      initialRouteName: "AuthLoading"
     }
   )
-
 );
